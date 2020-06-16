20dca50788d94e95ef9c363912ddfc7d5a2cd000
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-06-16 14:51:03 +0200

    Add film array

---
+++ src/films.js
@@ -0,0 +1,24 @@
+/* Movies */
+let movies = [
+`Donnie Darko`,
+`The Fountain`,
+`Requiem for a Dream`,
+`Moulin Rouge`,
+`Hotel Rwanda`,
+`V for Vendetta`,
+`The Sixth Sense`,
+`The Wolf of Wall Street`,
+`The Prestige`,
+`Office Space`,
+`Pans Labyrinth`,
+`Get Out`,
+`Inside Out`,
+`Choke`,
+`Dead Poets Society`,
+`Riding in Cars with Boys`,
+`Hail Satan`,
+`Borat: Cultural Learnings Of America For Make Benefit Glorious Nation Of Kazakhstan`,
+`Eternal Sunshine of the Spotless Mind`,
+`Confessions of a Shopaholic`,
+];
+
