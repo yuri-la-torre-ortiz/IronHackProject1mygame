26f555edade5c3fb50954140d926471d9577bc44
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-06-16 17:00:18 +0200

    Refactor Game function

--- src/main.js
+++ src/main.js
@@ -1,35 +1,35 @@
-
-
-
-//Build Movie: Image object
-function movieObject (movieArray){
-  let movie = movieArray.map(function (el){
-    return {title: el, image: el + ".jpg"}
-  })
-  return movie
-}
-
-// Select random film
-function randomSelect(movieArray) {
-  let movie = movieObject(movieArray);
-  let max = (movieArray.length - 1); 
-  let randomNumber = Math.floor(Math.random() * (max));
-  return movie[randomNumber]
-}
-
-
-//Guess function
 let array = [];
 let attempt = 0;
 let counter = 0;
+
+function movieObject() {
+  const movie = movies.map(function (el) {
+    return { title: el, image: el + ".jpg" };
+  });
+  return movie;
+}
+function Game(){
+console.log(movieObject());
+/* function randomSelect() {
+  let film = movieObject();
+  let max = (movie.length - 1); 
+  let randomNumber = Math.floor(Math.random() * (max));
+  let film = movie[randomNumber];
+  console.log(film);
+  }; */
+}
+
+Game();
+
 function guess(character) {
-    let word = "osmosis";
-    if (word.includes(character)){
-    for (let i = 0; i < word.length; i++) {
-      if (character === word[i]){
-        array[i] = word[i];
+  
+  const title = randomSelect();
+  if (title.title.toLowerCase().includes(character)){
+    for (let i = 0; i < title.length; i++) {
+      if (character === title[i]){
+        array[i] = title[i];
         counter++;
-        if (counter === word.length) return "You win!!!"
+        if (counter === title.length) return "You win!!!"
       } 
     }
     } else {
@@ -40,4 +40,3 @@ function guess(character) {
     let newWord = array.join();
     return newWord    
   }
-
