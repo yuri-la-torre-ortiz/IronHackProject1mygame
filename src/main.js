578b567d52b86e4e52b817fad1033253b4fc96fe
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-06-17 19:53:32 +0200

    Update project

--- src/main.js
+++ src/main.js
@@ -1,42 +1,89 @@
+let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let alphaArr = alphabet.split('');
+let alphabetDiv = document.querySelector(".alphabet");
+let hiddenTitle = document.querySelector(".hiddenWord");
 let array = [];
-let attempt = 0;
-let counter = 0;
 
-function movieObject() {
+// function 
+// document.write("<img src = "assets/A-wood.jpg" height="30" width="30">");
+function renderCorrectGuesses(letter) {
+  let span = document.createElement("span");
+  span.innerText = letter;
+  console.log(letter)
+  hiddenTitle.appendChild(span);
+}
+
+
+
+function createBtn(letter) {
+  let btn = document.createElement("button");
+  btn.innerText = letter;
+  btn.addEventListener("click", () => guess(letter))
+  alphabetDiv.appendChild(btn);
+}
+alphaArr.forEach((letter) => {
+  createBtn(letter);
+});
+
+function movieObject(movies) {
   const movie = movies.map(function (el) {
     return { title: el, image: el + ".jpg" };
   });
   return movie;
 }
+
+let mappedArray = movieObject(movies);
+
 function Game(){
-console.log(movieObject());
-/* function randomSelect() {
-  let film = movieObject();
-  let max = (movie.length - 1); 
+  //Select random film from array
+
+}
+// movieObject(movies);
+function randomSelect(movies) {
+  let max = (movies.length - 1); 
   let randomNumber = Math.floor(Math.random() * (max));
-  let film = movie[randomNumber];
-  console.log(film);
-  }; */
+  let word = movies[randomNumber].title;
+  word = word.toUpperCase();
+  let guessedLetters = [];  //Collect guessedLetters
+  let maskedLetters = "";  
+  return word;
 }
 
-Game();
+const selectedMovie = randomSelect(mappedArray);
 
+let attempt = 0;
+let counter = 0;
 function guess(character) {
-  
-  const title = randomSelect();
-  if (title.title.toLowerCase().includes(character)){
-    for (let i = 0; i < title.length; i++) {
-      if (character === title[i]){
-        array[i] = title[i];
+  console.log("here", character, selectedMovie);
+  if (selectedMovie.includes(character)){
+    for (let i = 0; i < selectedMovie.length; i++) {
+      if (character === selectedMovie[i]){
+        array[i] = selectedMovie[i];
         counter++;
-        if (counter === title.length) return "You win!!!"
+        console.log(array, "this is the array", counter);
+        console.log(document.querySelector(`.index${i}`));
+        document.querySelector(`.index${i}`).innerText = selectedMovie[i];
+        //renderCorrectGuesses(character);
+        if (counter === selectedMovie.length) return "You win!!!"
       } 
     }
     } else {
       attempt++;
+      console.log(attempt);
       if (attempt >= 7) return `Game Over.  You're dead.`
       return attempt;
     }
     let newWord = array.join();
+    console.log(newWord);
     return newWord    
   }
+
+  selectedMovie.split('').forEach((letter, i) => {
+    let span = document.createElement("span");
+    span.innerText = "_";
+    span.classList.add(`index${i}`)
+    hiddenTitle.appendChild(span);
+    if(letter === " "){
+      span.innerText = " ";
+    }
+  })
