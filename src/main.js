1fe91066efd121b255c193e9f03047c68edfc759
Yuri La Torre Ortiz <yuri.la.torre.ortiz@protonmail.ch>
2020-06-18 16:20:48 +0200

    Sort out guess function

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 let alphaArr = alphabet.split('');
 let alphabetDiv = document.querySelector(".alphabet");
 let hiddenTitle = document.querySelector(".hiddenWord");
-let array = [];
+let guessedLetters = [];  //Collect guessedLetters
+let maskedWord = ""; 
+let incorrectGuesses = 0;
+let mappedArray = movieObject(movies);
+const selectedMovie = randomSelect(mappedArray);
 
 // function 
-// document.write("<img src = "assets/A-wood.jpg" height="30" width="30">");
 function renderCorrectGuesses(letter) {
   let span = document.createElement("span");
   span.innerText = letter;
@@ -13,11 +16,10 @@ function renderCorrectGuesses(letter) {
   hiddenTitle.appendChild(span);
 }
 
-
-
 function createBtn(letter) {
   let btn = document.createElement("button");
   btn.innerText = letter;
+  btn.setAttribute('id', letter);
   btn.addEventListener("click", () => guess(letter))
   alphabetDiv.appendChild(btn);
 }
@@ -32,50 +34,48 @@ function movieObject(movies) {
   return movie;
 }
 
-let mappedArray = movieObject(movies);
 
-function Game(){
-  //Select random film from array
-
-}
-// movieObject(movies);
+function removeBtn(letter) {
+  var btn = document.getElementById(letter);
+  btn.remove();
+}  
+// Randomly select movieObject(movies);
 function randomSelect(movies) {
   let max = (movies.length - 1); 
   let randomNumber = Math.floor(Math.random() * (max));
   let word = movies[randomNumber].title;
-  word = word.toUpperCase();
-  let guessedLetters = [];  //Collect guessedLetters
-  let maskedLetters = "";  
+  word = word.toUpperCase(); 
   return word;
 }
 
-const selectedMovie = randomSelect(mappedArray);
-
+const selectedMovieLetters = selectedMovie.split(' ').filter(Boolean).join('');
 let attempt = 0;
 let counter = 0;
+  
 function guess(character) {
-  console.log("here", character, selectedMovie);
+  removeBtn(character);
   if (selectedMovie.includes(character)){
     for (let i = 0; i < selectedMovie.length; i++) {
       if (character === selectedMovie[i]){
-        array[i] = selectedMovie[i];
+        guessedLetters[i] = selectedMovie[i];
         counter++;
-        console.log(array, "this is the array", counter);
-        console.log(document.querySelector(`.index${i}`));
         document.querySelector(`.index${i}`).innerText = selectedMovie[i];
         //renderCorrectGuesses(character);
-        if (counter === selectedMovie.length) return "You win!!!"
-      } 
+      }
     }
-    } else {
+  } else {
       attempt++;
-      console.log(attempt);
-      if (attempt >= 7) return `Game Over.  You're dead.`
-      return attempt;
-    }
-    let newWord = array.join();
-    console.log(newWord);
-    return newWord    
+      console.log(`You are ${attempt} out of 7 attempts closer to death.`);
+  }
+  if (attempt === 7) {
+    return console.log(`Game Over.  You're dead.`)
+  }
+  let newWord = guessedLetters.filter(Boolean).join('');
+  if (newWord === selectedMovieLetters){
+    return console.log(`You win!!!`);
+  }
+  console.log(newWord);
+  return newWord    
   }
 
   selectedMovie.split('').forEach((letter, i) => {
